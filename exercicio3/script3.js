7a6ff4e446a70fe0a671d3c2770c4da8abcd6389
lehfalcade <letfalcade@gmail.com>
2025-05-27 12:24:13 -0300

    Add files via upload

--- exercicio3/script3.js
+++ exercicio3/script3.js
@@ -1,17 +1,20 @@
+let pontosfaltando
+var nome = Number(window.prompt("Digite aqui o seu nome:"))
 var nota1 = Number(window.prompt("Digite aqui a primeira nota:"));
-var nota2 = Number(window.prompt("Digite a segunda nota:"));
-var nota3 = Number(window.prompt("Digite aqui a terceira nota:"))
+var nota2 = Number(window.prompt("Digite aqui a segunda nota:"));
+var nota3 = Number(window.prompt("Digite aqui a terceira nota:"));
 
 let total = (nota1 + nota2 + nota3) / 3;
 
+alert("Média final: " + total);
 
-alert("Média final: " + total)
-
-if (total => 60){
-    alert("Você atingiu a média. " + total);
-    alert("Aprovado!")
+if (total => 6.0){
+    alert("Parabéns " + nome + " você atingiu a média com: " + total);
+    alert("Aprovado!");
 } else {
-    pontosfaltando = 60 - resultado
-        alert ("Reprovado, faltaram: " + pontosfaltando + " pontos.");
+    pontosfaltando = 6.0 - total;
+        alert ("Reprovado " + nome + " você atingiu: " + total);
+        alert ("Confira esse link para conseguir aprender mais: " + "https://pt.khanacademy.org/")
 }
+
 
