79ba755d986cfe950280ae30ac2baf26680a2664
lehfalcade <letfalcade@gmail.com>
2025-06-10 08:49:18 -0300

    Add files via upload

---
+++ exercicio5/script5.js
@@ -0,0 +1,11 @@
+function Digite(){
+let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+let num2 = Number(window.prompt("Digite aqui o segundo número:"));
+}
+
+if(num1 > num2){
+    alert("O primeiro número é maior que o segundo.")
+}else{
+    alert("O segundo número é maior que o primeiro.")
+}
+
