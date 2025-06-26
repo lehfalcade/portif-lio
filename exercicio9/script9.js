57149d131984a9816e29d96a47f0f2b7abd9ed2c
lehfalcade <letfalcade@gmail.com>
2025-06-26 10:46:48 -0300

    Add files via upload

--- exercicio9/script9.js
+++ exercicio9/script9.js
@@ -1,17 +1,42 @@
+let mostraHTML = document.getElementById("mostraHTML");
 function estacao(){
-    let texto = prompt("Digite um mês do ano: ").toUpperCase();
-    console.log(texto);
-    
-    if (texto == janeiro){
-        alert("Verão!")
-    }else{
-        if (texto == fevereiro){
-       alert("Verão!")
-    }else{
-        if (texto == março){
-            alert("Outono!")
-        }else{
-              
-        }
+    let mes = prompt("Digite um mês do ano: ").toUpperCase();
+
+    console.log(mes);
+    if(mes == "JANEIRO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é verão! </p>`
+    }
+    if(mes == "FEVEREIRO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é verão! </p>`
+    }
+    if(mes == "MARÇO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é outono! </p>`
+    }
+    if(mes == "ABRIL"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é outono! </p>`
+    }
+    if(mes == "MAIO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é outono! </p>`
+    }
+    if(mes == "JUNHO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é inverno! </p>`
+    }
+    if(mes == "JULHO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é inverno! </p>`
+    }
+    if(mes == "AGOSTO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é inverno! </p>`
+    }
+    if(mes == "SETEMBRO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é primavera! </p>`
+    }
+    if(mes == "OUTUBRO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é primavera! </p>`
+    }
+    if(mes == "NOVEMBRO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é primavera! </p>`
+    }
+    if(mes == "DEZEMBRO"){
+    mostraHTML.innerHTML = `<p>Você falou o ${mes}. A estação do ano é verão! </p>`
     }
 }
