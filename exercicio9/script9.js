1fbac6c000184abd06adf820f261e61de56bad9c
lehfalcade <letfalcade@gmail.com>
2025-06-26 09:38:53 -0300

    Add files via upload

---
+++ exercicio9/script9.js
@@ -0,0 +1,17 @@
+function estacao(){
+    let texto = prompt("Digite um mês do ano: ").toUpperCase();
+    console.log(texto);
+    
+    if (texto == janeiro){
+        alert("Verão!")
+    }else{
+        if (texto == fevereiro){
+       alert("Verão!")
+    }else{
+        if (texto == março){
+            alert("Outono!")
+        }else{
+              
+        }
+    }
+}
